f204cbeb3894cbab3d1cc54cb60bed6f42fdf370
Hormoz Halimi <hormoz_halimi@yahoo.com>
2023-01-02 19:44:59 -0800

    Update EmailService.java
    Coding

--- myTube/EmailService.java
+++ myTube/EmailService.java
@@ -4,6 +4,7 @@ package com.mytube;
 public class EmailService implements NotificationService {
     @Override
     public void notify(User user) {
+        
         System.out.println("Notifying " + user.getEmail() + "...");
         System.out.println("Done!\n");
     }
